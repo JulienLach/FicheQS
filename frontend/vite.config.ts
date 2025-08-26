8cea7fa039fee0f4a8db86feee1fa0e2d666b544
JulienLach <leadedchamps@gmail.com>
2025-08-26 22:48:19 +0200

    fix vite config with env variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        proxy: {
-            "/api": "http://localhost:3001",
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, "../", "VITE_");
+
+    return {
+        plugins: [react()],
+        server: {
+            port: 3000,
+            proxy: {
+                "/api": "http://localhost:3001",
+            },
         },
-    },
+        define: {
+            "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+        },
+    };
 });
