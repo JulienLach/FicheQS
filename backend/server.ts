23a5a306b0cdaae47a13a3259c41a8597b214132
JulienLach <leadedchamps@gmail.com>
2025-08-28 07:40:35 +0200

    feat(sanitize): add DOMPurify and jsdom dependencies and create sanitize middleware

--- backend/server.ts
+++ backend/server.ts
@@ -18,7 +18,7 @@ if (!ORIGIN_URL) {
     throw new Error("ORIGIN_URL missing");
 }
 
-const allowedOrigins = [ORIGIN_URL, "http://localhost:3000", "http://192.168.10.102:3000"];
+const allowedOrigins = [ORIGIN_URL, "http://localhost:3000"];
 
 app.use(
     cors({
