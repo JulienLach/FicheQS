f5e3bdad0b446053f94228c0e187208930f77d4f
JulienLach <leadedchamps@gmail.com>
2025-08-31 11:51:12 +0200

    feat(PWA): add vite-plugin-pwa config, fixe index.html to enable PWA  and remove classic PWA files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,48 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, "../", "VITE_");
 
     return {
-        plugins: [react()],
+        plugins: [
+            react(),
+            VitePWA({
+                registerType: "autoUpdate",
+                includeAssets: ["favicon.ico", "icon-192x192.png", "icon-512x512.png"],
+                manifest: {
+                    name: "FicheQS",
+                    short_name: "FicheQS",
+                    start_url: "/",
+                    display: "standalone",
+                    background_color: "#ffffff",
+                    theme_color: "#00DD80",
+                    icons: [
+                        {
+                            src: "icon-192x192.png",
+                            sizes: "192x192",
+                            type: "image/png",
+                            purpose: "any maskable",
+                        },
+                        {
+                            src: "icon-512x512.png",
+                            sizes: "512x512",
+                            type: "image/png",
+                            purpose: "any maskable",
+                        },
+                    ],
+                },
+                // Version simplifiée de Workbox
+                workbox: {
+                    globPatterns: ["**/*.{js,css,html,ico,png,jpg,svg}"],
+                },
+                devOptions: {
+                    enabled: true,
+                },
+            }),
+        ],
         server: {
             port: 3000,
             proxy: {
