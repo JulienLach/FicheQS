2474b8d66584862cedcc4c3048d36fba6c9915c4
JulienLach <leadedchamps@gmail.com>
2025-09-07 10:43:25 +0200

    update docs for CD.yml ansible test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
 
+# 1.0.3 (2025-09-07)
+
 # 1.0.2 (2025-09-07)
 
 ## Fixes
--- backend/updates/migration.ts
+++ backend/updates/migration.ts
@@ -15,7 +15,7 @@ export async function runMigrations(): Promise<void> {
         // Liste des migrations à appliquer
         const migrations: { version: string; updateDatabase: () => Promise<void> }[] = [
             // {
-            //     version: "1.0.2",
+            //     version: "1.0.3",
             //     updateDatabase: async () => {
             //         // Insérer les migrations ici
             //     },
